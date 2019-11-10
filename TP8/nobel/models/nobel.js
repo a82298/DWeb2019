3be402679e967662fa631c0529b517bd61d85d8a
Luís Martins <36774095+a82298@users.noreply.github.com>
2019-11-10 17:59:33 +0000

    Add files via upload

--- TP8/nobel/models/nobel.js
+++ TP8/nobel/models/nobel.js
@@ -1,1 +1,19 @@
+const mongoose = require('mongoose')
 
+var Person = new mongoose.Schema({
+    _id: {type: Number, required: true},
+    firstname : String,
+    surname : String,
+    motivation: String,
+    share : Number
+})
+
+var nobelSchema = new mongoose.Schema({
+    _id: {type: String, required: true},
+    year: {type: Number, required: true},
+    category: {type: String, required: true},
+    overallMotivation: String,
+    laureates: [Person]
+  });
+
+module.exports = mongoose.model('prizes', nobelSchema)
